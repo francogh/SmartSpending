28c8a920038636588c7fad89255aeefaa61e9e0a
Franco Ghiotti <francoghiotti@gmail.com>
2021-09-26 13:00:17 -0300

    para entregable

--- js/functions.js
+++ js/functions.js
@@ -1,36 +1,221 @@
+
+
+// FECHA DEL DIA
+window.onload = function () {
+  let fecha = new Date(); //Fecha actual
+  let mes = fecha.getMonth() + 1; //obteniendo mes
+  let dia = fecha.getDate(); //obteniendo dia
+  let ano = fecha.getFullYear(); //obteniendo año
+
+  if (dia < 10) dia = "0" + dia; //agrega cero si la hora menor de 10
+  if (mes < 10) mes = "0" + mes; //agrega cero si los minutos menor de 10
+  document.getElementById("fechaActual").value = ano + "-" + mes + "-" + dia;
+};
+
 const addIn = () => {
-  let adddate = prompt("Ingrese una fecha");
-  let rubro = prompt("Cual es el concepto del Ingreso?");
-  let importe = parseFloat(prompt("Ingrese el monto"));
-  inOutt.push({ inOut: adddate, rubro: rubro, importe: importe });
-  console.log('REGISTRASTE UN NUEVO INGREO' + '\nFecha: ' + adddate + '\nConcepto: ' + rubro + '\nimporte: ' + importe);
-  saldo = saldo + importe;
-  console.log('Tu nuevo saldo es de: ' + saldo);
+  let adddate = document.getElementById("fechaActual").value;
+  let rubro = document.getElementById("conceptoDineroIngresar").value;
+  let importe = parseFloat(document.getElementById("dineroAIngresar").value);
+  let trans = "I";
+  let tipo = "";
+  let id = inOutt.length + 1;
+  inOutt.push({
+    id: id,
+    inOut: adddate,
+    rubro: rubro,
+    importe: importe,
+    trans: trans,
+    tipo: tipo,
+  });
+  saldoAcumulado = saldoAcumulado + importe;
+
+  let dinero = document.getElementById("dineroDisponible");
+
+  dinero.innerHTML = saldoAcumulado;
+  document.getElementById("conceptoDineroIngresar").value = "";
+  document.getElementById("dineroAIngresar").value = "";
 };
 
 const addOut = () => {
-    let adddate = prompt("Ingrese una fecha");
-    let rubro = prompt("Cual es el concepto del gasto gasto?");
-    let importe = parseFloat(prompt("Ingrese el monto"));
-    inOutt.push({ inOut: adddate, rubro: rubro, importe: importe });
-    console.log('REGISTRASTE UN NUEVO GASTO' + '\nFecha: ' + adddate + '\nConcepto: ' + rubro + '\nimporte: ' + importe);
-    saldo = saldo - importe;
-    console.log('Tu nuevo saldo es de: ' + saldo);
-  };
+  let adddate = document.getElementById("fechaActual").value;
+  let rubro = document.getElementById("origenDelGasto").value;
+  let importe = parseFloat(document.getElementById("montoAQuitar").value);
+  let trans = "E";
+  let tipo = document.getElementById("selectorDeRubros").value;
+  let id = inOutt.length + 1;
+  inOutt.push({
+    id: id,
+    inOut: adddate,
+    rubro: rubro,
+    importe: importe,
+    trans: trans,
+    tipo: tipo,
+  });
+  saldoAcumulado = saldoAcumulado - importe;
 
+  let dinero = document.getElementById("dineroDisponible");
 
-
-  
-const incomeExpenses = () =>{
-  const incomeExpenses = prompt("desea ingresar otro movimiento? \nS para SI/ N para NO");
-  return incomeExpenses;
-
+  dinero.innerHTML = saldoAcumulado;
+  document.getElementById("origenDelGasto").value = "";
+  document.getElementById("montoAQuitar").value = "";
 };
-const movement = () =>{
-  const movementCuestion = prompt("Si es un ingreso coloque I \nSi es un gasto coloque G");
-  return movementCuestion;
 
-};
+function actualizarDinero() {
+  for (let index = 0; index < inOutt.length; index++) {
+    let agregarATabla = document.getElementById("tablaDeMoviemientos");
+    if (inOutt[index].trans === "E") {
+      agregarATabla.innerHTML += `<tr id="ingresoRojo">
+                                <th scope="row" class="hiddDate">${inOutt[index].inOut}</th>
+                                <td>${inOutt[index].rubro}</td>
+                                <td>$ ${inOutt[index].importe}</td>
+                                <td><button type="button" class="btn btn-danger">
+                                <i class="fas fa-trash-alt" onclick="deleteRow(this)"></i>
+                              </button></td>
+                              </tr>`;
+    } else {
+      agregarATabla.innerHTML += `<tr id="ingresoVerde">
+                                <th scope="row" class="hiddDate">${inOutt[index].inOut}</th>
+                                <td>${inOutt[index].rubro}</td>
+                                <td>$ ${inOutt[index].importe}</td>
+                                <td><button type="button" class="btn btn-danger">
+                                <i class="fas fa-trash-alt" onclick="deleteRow(this)"></i>
+                              </button></td>
+                              </tr>`;
+    }
+  }
+}
+
+function nuevoMovimientoIngreso() {
+  for (let index = inOutt.length - 1; index < inOutt.length; index++) {
+    let agregarATabla = document.getElementById("tablaDeMoviemientos");
+
+    agregarATabla.innerHTML += `<tr id="ingresoVerde">
+                                  <th scope="row" class="hiddDate">${inOutt[index].inOut}</th>
+                                  <td>${inOutt[index].rubro}</td>
+                                  <td>$ ${inOutt[index].importe}</td>
+                                  <td><button type="button" class="btn btn-danger">
+                                <i class="fas fa-trash-alt" onclick="deleteRow(this)"></i>
+                              </button></td>
+                                </tr>`;
+  }
+}
+function nuevoMovimientoIngresoSalida() {
+  for (let index = inOutt.length - 1; index < inOutt.length; index++) {
+    let agregarATabla = document.getElementById("tablaDeMoviemientos");
+
+    agregarATabla.innerHTML += `<tr id="ingresoRojo">
+                                    <th scope="row" class="hiddDate">${inOutt[index].inOut}</th>
+                                    <td>${inOutt[index].rubro}</td>
+                                    <td>$ ${inOutt[index].importe}</td>
+                                    <td><button type="button" class="btn btn-danger">
+                                  <i class="fas fa-trash-alt" onclick="deleteRow(this)"></i>
+                                </button></td>
+                                  </tr>`;
+  }
+}
+
+function deleteRow(btn) {
+  var row = btn.parentNode.parentNode.parentNode;
+  row.parentNode.removeChild(row);
+}
+
+function barradegastos() {
+  let dineroSal = 0;
+  let dineroEnt = 0;
+  let barraIngresos = 0;
+  let barraEgresos = 0;
+
+  for (let index = 0; index < inOutt.length; index++) {
+    if (inOutt[index].trans === "E") {
+      dineroSal += inOutt[index].importe;
+    } else {
+      dineroEnt += inOutt[index].importe;
+    }
+  }
+
+  saldoAcumulado = dineroEnt - dineroSal;
+
+  let dinero = document.getElementById("dineroDisponible");
+  dinero.innerHTML = saldoAcumulado;
+}
+
+let comidaBarra = 0;
+let transporteBarra = 0;
+let viviendaBarra = 0;
+let hobbiesBarra = 0;
+let serviciosBarra = 0;
+let variosBarra = 0;
+function progresoGastos() {
+  for (let index = 0; index < inOutt.length; index++) {
+    if (inOutt[index].tipo === "comida") {
+      comidaBarra += inOutt[index].importe;
+    } else if (inOutt[index].tipo === "transporte") {
+      transporteBarra += inOutt[index].importe;
+    } else if (inOutt[index].tipo === "vivienda") {
+      viviendaBarra += inOutt[index].importe;
+    } else if (inOutt[index].tipo === "hobbies") {
+      hobbiesBarra += inOutt[index].importe;
+    } else if (inOutt[index].tipo === "servicios") {
+      serviciosBarra += inOutt[index].importe;
+    } else if (inOutt[index].tipo === "varios") {
+      variosBarra += inOutt[index].importe;
+    }
+  }
+
+  gastosTotales =
+    transporteBarra +
+    comidaBarra +
+    viviendaBarra +
+    hobbiesBarra +
+    serviciosBarra +
+    variosBarra;
+    console.log(gastosTotales);
+  comidaBarra = parseInt((comidaBarra * 100) / gastosTotales);
+  transporteBarra = parseInt((transporteBarra * 100) / gastosTotales);
+  viviendaBarra = parseInt((viviendaBarra * 100) / gastosTotales);
+  hobbiesBarra = parseInt((hobbiesBarra * 100) / gastosTotales);
+  serviciosBarra = parseInt((serviciosBarra * 100) / gastosTotales);
+  variosBarra = parseInt((variosBarra * 100) / gastosTotales);
   
 
- 
+  let barComida = document.getElementById("barComida");
+  barComida.style.width = comidaBarra + "%";
+
+  let barTransporte = document.getElementById("barTransporte");
+  barTransporte.style.width = transporteBarra + "%";
+
+  let barVivienda = document.getElementById("barVivienda");
+  barVivienda.style.width = viviendaBarra + "%";
+
+  let barHobbies = document.getElementById("barHobbies");
+  barHobbies.style.width = hobbiesBarra + "%";
+
+  let barServicios = document.getElementById("barServicios");
+  barServicios.style.width = serviciosBarra + "%";
+
+  let barVarios = document.getElementById("barVarios");
+  barVarios.style.width = variosBarra + "%";
+}
+
+function borrarObjeto(i) {
+  let obj = i;
+  console.log(inOutt.obj);
+}
+
+actualizarDinero();
+let saldoAcumulado = 0;
+barradegastos();
+progresoGastos();
+
+function agregarDinero() {
+  addIn();
+  nuevoMovimientoIngreso();
+  //agregar el movimiento a la tabla
+  progresoGastos();
+}
+
+function sacarDinero() {
+  addOut();
+  nuevoMovimientoIngresoSalida();
+  progresoGastos();
+}
--- js/main.js
+++ js/main.js
@@ -1,48 +1,42 @@
-// FECHA DEL DIA
-window.onload = function () {
-  let fecha = new Date(); //Fecha actual
-  let mes = fecha.getMonth() + 1; //obteniendo mes
-  let dia = fecha.getDate(); //obteniendo dia
-  let ano = fecha.getFullYear(); //obteniendo año
 
-  if (dia < 10) dia = "0" + dia; //agrega cero si la hora menor de 10
-  if (mes < 10) mes = "0" + mes; //agrega cero si los minutos menor de 10
-  document.getElementById("fechaActual").value = ano + "-" + mes + "-" + dia;
-};
 
-let inOutt = [
-  { inOut: "10/10/21", rubro: "Colectivo", importe: 100 },
-  { inOut: "14/12/21", rubro: "Alquiler", importe: 300 },
-  { inOut: "12/02/21", rubro: "Nafta", importe: 1220 },
-  { inOut: "22/06/21", rubro: "Supermercado", importe: 101 },
-  { inOut: "28/12/21", rubro: "Herramienta", importe: 1002 },
-  { inOut: "02/09/21", rubro: "Perfume", importe: 1992 },
-  { inOut: "01/10/21", rubro: "Remera", importe: 10113 },
-];
 
-const salida = inOutt.filter((anm) => {
-  return anm.rubro !== "Remera";
-});
 
-let saldo = parseInt(prompt("Ingrese su sado inicial"));
-console.log(saldo);
-let cuestion = prompt("Desea ingresar un mivimiento? \nS para SI / N para NO");
+// const salida = inOutt.filter((anm) => {
+//   return anm.rubro !== "Remera";
+// });
 
-if (cuestion === "S" || cuestion === "s") {
-  do {
-    var moveme = movement();
+// let saldo = parseInt(prompt("Ingrese su sado inicial"));
+// console.log(saldo);
+// let cuestion = prompt("Desea ingresar un mivimiento? \nS para SI / N para NO");
 
-    if (moveme === "i" || moveme === "I") {
-      addIn();
-    } else if (moveme === "g" || moveme === "G") {
-      addOut();
-    } else {
-      alert("No es una opcion valida!");
-    }
-    income = incomeExpenses();
+// if (cuestion === "S" || cuestion === "s") {
+//   do {
+//     var moveme = movement();
 
-  } while (income === "S" || income === "s");
-  console.log("Gracias por utilizar Smart Spending");
-} else {
-  alert("Gracias por utilizar Smart Spending");
-}
+//     if (moveme === "i" || moveme === "I") {
+//       addIn();
+//     } else if (moveme === "g" || moveme === "G") {
+//       addOut();
+//     } else {
+//       alert("No es una opcion valida!");
+//     }
+//     income = incomeExpenses();
+
+//   } while (income === "S" || income === "s");
+//   console.log("Gracias por utilizar Smart Spending");
+// } else {
+//   alert("Gracias por utilizar Smart Spending");
+// }
+
+//PIDO QUE INGRESE EL SALDO
+// let saldo = parseInt(prompt("Ingrese su sado inicial"));
+// console.log(saldo);
+
+
+// //GUARDO EL SALDO EN EL P
+// let dinero = document.getElementById('dineroDisponible');
+// dinero.innerHTML = saldo;
+
+
+
